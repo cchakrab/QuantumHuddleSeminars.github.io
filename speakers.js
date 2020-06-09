ed88b6c2e6db5d1cbcc3a15c74a4e5c719e0a55d
Stefan Krastanov <stefan@krastanov.org>
2020-06-09 18:00:38 -0400

    Automate speaker listing and set up rough structure.

---
+++ speakers.js
@@ -0,0 +1,16 @@
+SPEAKERS = [
+{
+"date": new Date("2020-07-06T12:00:00-04:00"),
+"name": "Javier Aizpurua",
+"affiliation": "CSM",
+"title": "This is the title",
+"abstract": "This is the abstract"
+},
+{
+"date": new Date("2020-07-13T12:00:00-04:00"),
+"name": "David Mazziotti",
+"affiliation": "ETH",
+"title": "This is the title",
+"abstract": "This is the abstract"
+}
+];
