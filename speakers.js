c23db8923291b155ed4dd90697afc8f818d33776
tomasneuman1990 <65783012+tomasneuman1990@users.noreply.github.com>
2020-06-14 13:46:55 -0400

    Speakers June/July

--- speakers.js
+++ speakers.js
@@ -1,23 +1,46 @@
 SPEAKERS = [
+  
+{
+"date": new Date("2020-06-22T12:00:00-04:00"),
+"name": "Mohammad Hafezi",
+"affiliation": "University of Maryland",
+"title": "Quantum optics toolbox for topological and correlated states of electrons",
+"abstract": "We describe how various quantum optics techniques can be exploited to engineer both single-particle and effective interaction terms in solid-state systems. We start by electronic quantum Hall systems and show how optical selection rules are modified and how an effective interaction can be generated using an optical drive. We also investigate whether anyonic excitation can be optically generated and manipulated. Moreover, we discuss how patterned light can lead to superlattice structures for electrons, including flatland, kagome lattice, etc. In the end, we discuss how optical manipulation, such as optical pumping and bath engineering, could provide a new way to enhance superconductivity."
+},
+{
+"date": new Date("2020-07-06T12:00:00-04:00"),
+"name": "Martin Frimmer",
+"affiliation": "ETH Zurich",
+"title": "TBA",
+"abstract": "TBA"
+},
 {
 "date": new Date("2020-07-06T12:00:00-04:00"),
 "name": "Javier Aizpurua",
-"affiliation": "CSM",
-"title": "This is the title",
-"abstract": "This is the abstract"
+"affiliation": "CFM",
+"title": "TBA",
+"abstract": "TBA"
 },
 {
 "date": new Date("2020-07-13T12:00:00-04:00"),
 "name": "David Mazziotti",
 "affiliation": "University of Chicago",
-"title": "This is the title",
-"abstract": "This is the abstract"
+"title": "TBA",
+"abstract": "TBA"
 },
+{
+"date": new Date("2020-07-27T12:00:00-04:00"),
+"name": "Mete Atatüre",
+"affiliation": "University of Cambridge",
+"title": "TBA",
+"abstract": "TBA"
+}
+];
 {
 "date": new Date("2020-07-27T12:00:00-04:00"),
 "name": "Danna Freedman",
 "affiliation": "Northwestern University",
-"title": "This is the title",
-"abstract": "This is the abstract"
+"title": "TBA",
+"abstract": "TBA"
 }
 ];
